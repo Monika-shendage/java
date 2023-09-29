7dce1bb0cfb78ecbdba23c60bb0e5d283762be0c
Monika-shendage <88097088+Monika-shendage@users.noreply.github.com>
2023-09-29 20:17:00 +0530

    Create array1.java

---
+++ array/array1.java
@@ -0,0 +1,10 @@
+package array;
+
+public class array1 {
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50};
+        for (int a:arr){
+            System.out.println(a);
+        }
+    }
+}
